d11d36642f1b22fae9b82961707218747e79e251
gabrielaleon <51418183+gabrielaleon@users.noreply.github.com>
2020-10-26 10:37:32 +0100

    Create evaluate.py

---
+++ w1/evaluation/evaluate.py
@@ -0,0 +1,32 @@
+from sklearn.metrics import *
+import numpy as np
+import scipy.spatial
+
+def evaluate_supervised(labels_true, labels_predicted):
+    contingency_matrix = cluster.contingency_matrix(labels_true, labels_predicted)
+    metrics_from_contingency_matrix = get_metrics_from_mat(contingency_matrix)
+    supervised_scores = dict(adjusted_mutual_info_score=adjusted_mutual_info_score(labels_true, labels_predicted, 'arithmetic'),
+               adjusted_rand_score=adjusted_rand_score(labels_true, labels_predicted),
+               completeness_score=completeness_score(labels_true, labels_predicted),
+               contingency_matrix=contingency_matrix,
+               fowlkes_mallows_score=fowlkes_mallows_score(labels_true, labels_predicted),
+               homogeneity_score=homogeneity_score(labels_true, labels_predicted),
+               v_measure_score=v_measure_score(labels_true, labels_predicted), **metrics_from_contingency_matrix)
+    return supervised_scores
+
+def evaluate_unsupervised(X, labels_predicted):
+    unsupervised_scores = dict(calinski_harabasz_score=calinski_harabasz_score(X, labels_predicted),
+                   davies_bouldin_score=davies_bouldin_score(X, labels_predicted),
+                   silhouette_score=silhouette_score(X, labels_predicted))
+    return unsupervised_scores
+
+
+def evaluate_soft_partitions(X, labels_true, lables_predicted, centroids):
+    contingency_matrix = cluster.contingency_matrix(labels_true, labels_predicted)
+    #calculations for xie_beni
+    centroids_dist = scipy.spatial.distance.cdist(centroids, centroids)**2
+    centroids_dist[centroids_dist == 0.0] = np.inf  
+    scores_soft_partitions = dict(normalized_partition_coefficient=(np.sum(contingency_matrix ** 2) / contingency_matrix.shape[1] - 1 / contingency_matrix.shape[0]) / (1 - 1 / contingency_matrix.shape[0]),
+               partition_entropy=-np.sum(contingency_matrix * np.log(contingency_matrix) / contingency_matrix.shape[1]),
+               xie_beni=np.sum((contingency_matrix**2).T*(scipy.spatial.distance.cdist(X, centroids)**2))/(X.shape[0]*np.min(centroids_dist)) )
+    return scores_soft_partitions
