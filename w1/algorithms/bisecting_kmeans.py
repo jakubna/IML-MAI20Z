c3224437334338069644bea1fb78289b2fad1e20
gabrielaleon <51418183+gabrielaleon@users.noreply.github.com>
2020-10-28 11:00:19 +0100

    Update bisecting_kmeans.py

--- w1/algorithms/bisecting_kmeans.py
+++ w1/algorithms/bisecting_kmeans.py
@@ -55,8 +55,8 @@ class Bisecting_KMeans:
         if self.centroids is None:
             raise Exception('Fit the model with some data before running a prediction')
 
-        distances = self._distances(X)
-        labels, self.nearest, nearest_ids = self._get_nearest(X, distances)
+        distances = KMeans._distances(X)
+        labels, self.nearest, nearest_ids = KMeans._get_nearest(X, distances)
 
         return labels
 
@@ -79,50 +79,6 @@ class Bisecting_KMeans:
         centroid = np.mean(cluster, 0)
         errors = np.linalg.norm(cluster-centroid, ord=2, axis=1)
         return np.sum(errors)
-                             
-    def _distances(self, X: np.ndarray):
-        """
-        Calculate distance from each point of the dataset to each cluster.
-        :param X: 2D data array of size (rows, features).
-        :return: Distance matrix of shape (K, number of points)
-        """
-        distances = np.zeros(shape=(self.k, X.shape[0]))
 
-        for centroid_id, centroid in enumerate(self.centroids):
-            for row_id, row in enumerate(X):
-                distances[centroid_id, row_id] = self._calculate_distance(centroid, row)
-
-        return distances
-
-    def _calculate_distance(self, x: np.ndarray, y: np.ndarray):
-        """
-        Calculate distance between 2 elements using the metric depending on the algorithm ('euclidean' or 'cityblock').
-        :param x: 1D vector with all x attributes.
-        :param y: 1D vector with all y attributes.
-        :return: Distance between both vectors using the specified metric.
-        """
-        return distance.cdist(np.array([x]), np.array([y]), metric=self.metric)[0][0]
-
-    def _get_nearest(self, X: np.ndarray, distances: np.ndarray):
-        """
-        Compute the distance for each dataset instance to each centroid.
-        :param X: 2D data array of size (rows, features).
-        :param distances: 2D vector of distances between centroids and points.
-        :return: Cluster indexes assigned to each observation (labels for each point).
-                 List of nearest observations for each cluster.
-                 List of nearest observations index for each cluster.
-        """
-        clusters = []
-        nearest = [[] for _ in range(self.k)]
-        nearest_id = [[] for _ in range(self.k)]
-
-        for row_id, row in enumerate(X):
-            cluster_id = int(np.argmin(distances[:, row_id]))
-
-            clusters.append(cluster_id)
-            nearest[cluster_id].append(row)
-            nearest_id[cluster_id].append(row_id)
-
-        return clusters, nearest, nearest_id
             
 
