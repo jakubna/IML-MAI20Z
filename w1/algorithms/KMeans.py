2a91edbee92f5221689ffcfe72ef2f8dfb7ea8fb
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-10-29 12:15:26 +0100

    Kmeans with standard pep8

--- w1/algorithms/KMeans.py
+++ w1/algorithms/KMeans.py
@@ -1,15 +1,18 @@
-import pandas as pd
 import numpy as np
-import random as rd
 from scipy.spatial import distance
+import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
+# only import sklearn for the metric of silhouette_score for reach the k optimal value.
+
 
 class KMeans:
-    def __init__(self, k: int, max_it=100, seed=1, tol=1e-5):
+    def __init__(self, k: int, max_it=100, seed=-1, tol=1e-5):
         """
         :param k: Number of Clusters
         :param max_it: Maximum number of iterations if hasn't reached convergence yet.
         :param seed: Fixed seed to allow reproducibility.
-        :param tol: Relative tolerance with regards difference in the cluster centers of two consecutive iterations to declare convergence.
+        :param tol: Relative tolerance with regards difference in the cluster centers of two consecutive iterations to
+        declare convergence.
         """
         if k < 1:
             raise ValueError('K must be a positive number')
@@ -24,59 +27,79 @@ class KMeans:
         self.X = None
         self.centroids = None
         self.nearest = None
-        self.inertia = None
 
-    def fit(self, X: np.ndarray):
+    def fit(self, x: np.ndarray):
         """
         Compute cluster centers and predict cluster index for each sample.
-        :param X: 2D data array of size (rows, features).
+        :param x: 2D data array of size (rows, features).
         """
-        np.random.seed(self.seed)
-        self.X = X
-        sse = []
+        if self.seed < 0:
+            np.random.seed()
+        else:
+            np.random.seed(self.seed)
+
+        self.X = x
         # Initialize centroids
         self._init_centroids()
 
         while True:
             self.n_it += 1
-            distances = self._distances(X)
-            labels, self.nearest, nearest_ids = self._get_nearest(X, distances)
+            distances = self._distances(x)
+            labels, self.nearest, nearest_ids = self._get_nearest(x, distances)
             self.previous_centroids = self.centroids.copy()
             self._update_centroids()
-            np_labels = np.array(labels)
-            # inertia calculus
-            for act in range(0, self.k):
-                index = np.where(np_labels == act)[0]
-                cluster_x = self.X[index, :]
-                se = np.sum(np.linalg.norm(cluster_x - self.centroids[act]))
-                sse.append(se)
-            self.inertia = np.min(sse)
 
             if self._check_convergence():
                 break
 
-    def predict(self, X: np.ndarray):
+    def predict(self, x: np.ndarray):
         """
         Assign labels to a list of observations.
-        :param X: 2D data array of size (rows, features).
+        :param x: 2D data array of size (rows, features).
         :return: Cluster indexes assigned to each row of X.
         """
         if self.centroids is None:
             raise Exception('Fit the model with some data before running a prediction')
 
-        distances = self._distances(X)
-        labels, self.nearest, nearest_ids = self._get_nearest(X, distances)
+        distances = self._distances(x)
+        labels, self.nearest, nearest_ids = self._get_nearest(x, distances)
 
         return labels
 
-    def fit_predict(self, X: np.ndarray):
+    def fit_predict(self, x: np.ndarray):
         """
         Fit the model with data and return assigned labels.
-        :param X: 2D data array of size (rows, features).
+        :param x: 2D data array of size (rows, features).
         :return: Cluster indexes assigned to each row of X.
         """
-        self.fit(X)
-        return self.predict(X)
+        self.fit(x)
+        return self.predict(x)
+
+    @staticmethod
+    def optimal_k4db(x: np.ndarray, k_range=range(2, 15), plot=False):
+        """
+        Search the optimal number of clusters for the X dataset.
+        :param x: 2D data array of size (rows, features).
+        :param k_range: the range of different k that the function will evaluate.
+        :param plot: this option pop up the Silhouette method plot.
+        :return: the optimal value of cluster for X data base.
+        """
+        if k_range.start < 2:
+            raise Exception('The minimum range starting number must be 2')
+        ss_distances = []
+        for k in k_range:
+            km = KMeans(k)
+            labels = km.fit_predict(x)
+            ss_distances.append(silhouette_score(x, labels, metric='euclidean'))
+
+        if plot:
+            plt.plot(k_range, ss_distances, 'bx-')
+            plt.xlabel('k')
+            plt.ylabel('Silhouette score')
+            plt.title('The Silhouette Method For Optimal k')
+            plt.show()
+
+        return ss_distances.index(max(ss_distances)) + k_range.start
 
     def _init_centroids(self):
         """Initialize centroids"""
@@ -91,20 +114,21 @@ class KMeans:
         cost = 0
         for k in range(self.k):
             cost += \
-            distance.cdist(np.array([self.centroids[k]]), np.array([self.previous_centroids[k]]), metric=self.metric)[
-                0][0]
+                distance.cdist(np.array([self.centroids[k]]), np.array([self.previous_centroids[k]]),
+                               metric=self.metric)[
+                    0][0]
         return cost
 
-    def _distances(self, X: np.ndarray):
+    def _distances(self, x: np.ndarray):
         """
         Calculate distance from each point of the dataset to each cluster.
-        :param X: 2D data array of size (rows, features).
+        :param x: 2D data array of size (rows, features).
         :return: Distance matrix of shape (K, number of points)
         """
-        distances = np.zeros(shape=(self.k, X.shape[0]))
+        distances = np.zeros(shape=(self.k, x.shape[0]))
 
         for centroid_id, centroid in enumerate(self.centroids):
-            for row_id, row in enumerate(X):
+            for row_id, row in enumerate(x):
                 distances[centroid_id, row_id] = self._calculate_distance(centroid, row)
 
         return distances
@@ -118,10 +142,10 @@ class KMeans:
         """
         return distance.cdist(np.array([x]), np.array([y]), metric=self.metric)[0][0]
 
-    def _get_nearest(self, X: np.ndarray, distances: np.ndarray):
+    def _get_nearest(self, x: np.ndarray, distances: np.ndarray):
         """
         Compute the distance for each dataset instance to each centroid.
-        :param X: 2D data array of size (rows, features).
+        :param x: 2D data array of size (rows, features).
         :param distances: 2D vector of distances between centroids and points.
         :return: Cluster indexes assigned to each observation (labels for each point).
                  List of nearest observations for each cluster.
@@ -131,7 +155,7 @@ class KMeans:
         nearest = [[] for _ in range(self.k)]
         nearest_id = [[] for _ in range(self.k)]
 
-        for row_id, row in enumerate(X):
+        for row_id, row in enumerate(x):
             cluster_id = int(np.argmin(distances[:, row_id]))
 
             clusters.append(cluster_id)
@@ -154,6 +178,3 @@ class KMeans:
             return True
         else:
             return False
-
-
-
