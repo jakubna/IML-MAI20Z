716aae292a97b1261699ea32c9adede20a66d2a2
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-10-29 12:24:17 +0100

    nursery pep8 update (fixed warnings)

--- w1/dataPreprocessing/nursery.py
+++ w1/dataPreprocessing/nursery.py
@@ -4,13 +4,14 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import os
 
+
 def preprocess():
     # load dataset
     f = os.path.join(os.path.dirname(os.path.abspath(__file__)), "datasets", "datasets", "nursery.arff")
     data, meta = arff.loadarff(f)
     dataset = np.array(data.tolist(), dtype=object)
 
-    column_names = meta.names() # list containing column names
+    column_names = meta.names()  # list containing column names
     # create a initial pandas dataframe
     df = pd.DataFrame(data=dataset, columns=column_names)
 
@@ -21,35 +22,39 @@ def preprocess():
             df[x].fillna(top_frequent_value, inplace=True)
 
     # split-out dataset
-    X = df.iloc[:,:-1]
-    Y = df.iloc[:, -1]
+    x = df.iloc[:, :-1].copy()
+    y = df.iloc[:, -1].copy()
 
-    def label_encoding(x, classes):
+    def label_encoding(value, classes: bytearray):
         """Function written based on label encoding rule
             Parameters
             ----------
-            x : value to interpret
+            value : value to interpret
             classes: array of classes
             Returns
             -------
             self : returns index of x value in classes array.
             """
-        return classes.index(x)
+        return classes.index(value)
+
+    label_true = list(y.iloc[:].apply(label_encoding, classes=[b'not_recom', b'recommend', b'very_recom',
+                                                               b'priority', b'spec_prior']))
 
     # apply label_encoding function on selected columns
-    X.iloc[:,1] = X.iloc[:,1].apply(label_encoding, classes=[b'proper', b'less_proper', b'improper', b'critical', b'very_crit'])
-    X.iloc[:,3] = X.iloc[:,3].apply(label_encoding, classes=[b'1', b'2', b'3', b'more'])
-    X.iloc[:,4] = X.iloc[:,4].apply(label_encoding, classes=[b'convenient', b'less_conv', b'critical'])
-    X.iloc[:,5] = X.iloc[:,5].apply(label_encoding, classes=[b'convenient', b'inconv'])
-    X.iloc[:,6] = X.iloc[:,6].apply(label_encoding, classes=[b'nonprob', b'slightly_prob', b'problematic'])
-    X.iloc[:,7] = X.iloc[:,7].apply(label_encoding, classes=[b'not_recom', b'recommended', b'priority'])
+    x.a2 = x.a2.apply(label_encoding, classes=[b'proper', b'less_proper', b'improper', b'critical',
+                                               b'very_crit'])
+    x.a4 = x.a4.apply(label_encoding, classes=[b'1', b'2', b'3', b'more'])
+    x.a5 = x.a5.apply(label_encoding, classes=[b'convenient', b'less_conv', b'critical'])
+    x.a6 = x.a6.apply(label_encoding, classes=[b'convenient', b'inconv'])
+    x.a7 = x.a7.apply(label_encoding, classes=[b'nonprob', b'slightly_prob', b'problematic'])
+    x.a8 = x.a8.apply(label_encoding, classes=[b'not_recom', b'recommended', b'priority'])
 
     # apply one-hot encoding using get_dummies function from pandas
-    X=pd.get_dummies(X, columns=['a1','a3'])
+    x = pd.get_dummies(x, columns=['a1', 'a3'])
 
     # fit MinMax scaler on data
-    norm = MinMaxScaler().fit(X)
+    norm = MinMaxScaler().fit(x)
     # transform data
-    X = norm.transform(X)
+    x = norm.transform(x)
 
-    return X
+    return x, label_true, df
