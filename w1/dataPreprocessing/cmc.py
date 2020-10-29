0c856b49dd43d9339379b50649f9885b0456eb84
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-10-29 12:23:05 +0100

    cmc pep8 update

--- w1/dataPreprocessing/cmc.py
+++ w1/dataPreprocessing/cmc.py
@@ -5,15 +5,16 @@ import os
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 
+
 def preprocess():
     # load dataset
     f = os.path.join(os.path.dirname(os.path.abspath(__file__)), "datasets", "datasets", "cmc.arff")
     data, meta = arff.loadarff(f)
     dataset = np.array(data.tolist(), dtype=object)
 
-    column_names = meta.names() # list containing column names
-    column_types = meta.types() # list containing column types
-    column_types_dict = dict(zip(column_names[:-1], column_types[:-1])) # dictionary containing column names and types
+    column_names = meta.names()  # list containing column names
+    column_types = meta.types()  # list containing column types
+    column_types_dict = dict(zip(column_names[:-1], column_types[:-1]))  # dictionary containing column names and types
     # create a initial pandas dataframe
     df = pd.DataFrame(data=dataset, columns=column_names)
 
@@ -27,21 +28,35 @@ def preprocess():
                 median = df[x].median()
                 df[x].fillna(median, inplace=True)
 
-    # create instance of labelencoder
-    labelencoder = LabelEncoder()
+    # create instance of label_encoder
+    label_encoder = LabelEncoder()
     # transform data
-    for k,v in column_types_dict.items():
+    for k, v in column_types_dict.items():
         if v == 'nominal':
-            labelencoder.fit(sorted(df[k].unique()))
-            df[k] = labelencoder.transform(df[k])
+            label_encoder.fit(sorted(df[k].unique()))
+            df[k] = label_encoder.transform(df[k])
 
     # split-out dataset
-    X = df.iloc[:,:-1]
-    Y = df.iloc[:, -1]
+    x = df.iloc[:, :-1].copy()
+    y = df.iloc[:, -1].copy()
+
+    def label_encoding(value, classes: bytearray):
+        """Function written based on label encoding rule
+            Parameters
+            ----------
+            value : value to interpret
+            classes: array of classes
+            Returns
+            -------
+            self : returns index of x value in classes array.
+            """
+        return classes.index(value)
+
+    label_true = list(y.iloc[:].apply(label_encoding, classes=[b'1', b'2', b'3']))
 
     # fit MinMax scaler on data
-    norm = MinMaxScaler().fit(X)
+    norm = MinMaxScaler().fit(x)
     # transform data
-    X = norm.transform(X)
+    x = norm.transform(x)
 
-    return X
+    return x, label_true, df
