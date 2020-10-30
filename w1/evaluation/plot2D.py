e7bce7078e9426bd025f80d20248b455fd082a3a
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-10-30 16:04:58 +0100

    Update plot2D.py

--- w1/evaluation/plot2D.py
+++ w1/evaluation/plot2D.py
@@ -4,27 +4,34 @@ import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
 
-def plot2D (X , y_predict, title:str):
-  """ the function aims to reduce the features to two dimensions using PCA method and plot the clusters
-      X: input dataset
-      y_predict: results labels
-      title: graphic title (example: adults dataset using Kmeans)"""
-  pca = PCA(n_components=2)
-  principalComponents = pca.fit_transform(X)
-  finalDf = pd.DataFrame(data = principalComponents
-               , columns = ['principal component 1', 'principal component 2'])
-  fig = plt.figure(figsize = (8,8))
-  ax = fig.add_subplot(1,1,1) 
-  ax.set_xlabel('Principal Component 1', fontsize = 15)
-  ax.set_ylabel('Principal Component 2', fontsize = 15)
-  ax.set_title('2 component PCA' + title, fontsize = 20)
-  targets = y_predict.unique()
-  colors = ['r', 'g', 'k', 'y','c','m']
-  for target, color in zip(targets,colors):
-      indicesToKeep = y_predict == target
-      ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-                  , finalDf.loc[indicesToKeep, 'principal component 2']
-                 , c = color
-                 , s = 50)
-  ax.legend(targets)
-  ax.grid()
+def plot2d(x: np.ndarray, y, titles):
+    """ the function aims to reduce the features to two dimensions using PCA method and plot the clusters
+        x: 2D data array of size (rows, features).
+        y_predict: results labels.
+        title: graphic title (example: adults dataset using KMeans).
+    """
+    fig = plt.figure(figsize=(20, 20))
+    for i in range(0, len(titles)):
+        y_predict = y[i]
+        pca = PCA(n_components=2)
+        principal_components = pca.fit_transform(x)
+        final_df = pd.DataFrame(data=principal_components,
+                                columns=['principal component 1', 'principal component 2'])
+        ax = fig.add_subplot(2, 2, i + 1)
+        ax.set_xlabel('Principal Component 1', fontsize=10)
+        ax.set_ylabel('Principal Component 2', fontsize=10)
+        ax.set_title('2 component PCA: ' + titles[i], fontsize=15)
+        targets = list(set(y_predict))
+        y_predict = pd.DataFrame(y_predict).iloc[:, -1]
+        colors = ['r', 'g', 'k', 'y', 'c', 'm']
+        for target, color in zip(targets, colors):
+            indices2keep = y_predict == target
+            ax.scatter(final_df.loc[indices2keep, 'principal component 1'],
+                       final_df.loc[indices2keep, 'principal component 2'],
+                       c=color,
+                       s=50)
+        ax.legend(targets)
+        ax.grid()
+    plt.show()
+
+
