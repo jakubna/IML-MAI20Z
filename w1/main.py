05594adcd14196a05a4c3c2e45a5b2886e6c5f11
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-10-31 18:38:59 +0100

    Update main.py

--- w1/main.py
+++ w1/main.py
@@ -7,7 +7,7 @@ def main():
     res = preprocess_database(database_name)
 
     # define the parameters
-    parameters = dict(k=2, max_it=100, seed=-1, tol=1e-5, eps=0.5, epsilon=0.01, m=2, optimal_k=False)
+    parameters = dict(k=2, max_it=100, seed=1, tol=1e-5, eps=0.5, epsilon=0.01, m=2, optimal_k=False)
 
     # apply all the algorithms
     result = apply_algorithms(res['db'], res['label_true'], res['data_frame'], parameters)
@@ -19,11 +19,13 @@ def main():
     # load results to a csv file
     result['our_df'].to_csv("results/"+database_name+"_algorithms_results", sep='\t', encoding='utf-8', index=False)
     result['dbscan_df'].to_csv("results/"+database_name+"_dbscan_results", sep='\t', encoding='utf-8', index=False)
-    print("The output files are created in results folder of this project")
+    print("\nThe CSV output files are created in results folder of this project\n")
 
-    # metodes auxiliars, find eps, optimal K, fitxer de optimize
+    # find optimal K value for each algorithm
+    optim_k_value(res, database_name, parameters['seed'])
 
 
 if __name__ == "__main__":
     main()
+
 
