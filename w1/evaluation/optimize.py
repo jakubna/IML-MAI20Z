4b52bec2124adb12fbb6329ca80a983c785e2a96
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-10-31 18:39:46 +0100

    Update optimize.py

--- w1/evaluation/optimize.py
+++ w1/evaluation/optimize.py
@@ -1,21 +1,20 @@
-from typing import Type, List
+from typing import List
 from w1.evaluation.evaluate import *
-from w1.algorithms.KMeans import KMeans
-from w1.algorithms.KMedians import KMedians
-from w1.algorithms.bisecting_kmeans import BisectingKMeans
 from w1.algorithms.FuzzyCMeans import FuzzyCMeans
-def optimize(x: np.ndarray, y: np.ndarray, algorithm, metric: str,
-             k_values: List[int], goal: str):
+
+
+def optimize(x: np.ndarray, y: np.ndarray, algorithm, metric: str, k_values: List[int], goal: str, seed: int):
     """
     Optimize K value for the same data, algorithm and metric.
     :param x: 2D data matrix of size (#observations, #features).
     :param y: 1D data matrix of size (#true_labels).
     :param algorithm: Algorithm class to instantiate.
-    :param algorithm_params: Extra parameters for the algorithm class.
+    :param seed: seed algorithm
     :param metric: Metric function used to evaluate - possible: adjusted_mutual_info_score,adjusted_rand_score,
                                                                     completeness_score, fowlkes_mallows_score,
-                                                                    homogeneity_score,v_measure_score,calinski_harabasz_score,
-                                                                    davies_bouldin_score,silhouette_score,normalized_partition_coefficient,
+                                                                    homogeneity_score,v_measure_score,
+                                                                    calinski_harabasz_score, davies_bouldin_score,
+                                                                    silhouette_score,normalized_partition_coefficient,
                                                                     partition_entropy, xie_beni
     :param k_values: List of `K` values to test.
     :param goal: `max` or `min` the metric (maximaze or minimize the metric)
@@ -23,12 +22,12 @@ def optimize(x: np.ndarray, y: np.ndarray, algorithm, metric: str,
     """
     results = []
     score = 0
-    assert goal in ['max', 'min'] 
+    assert goal in ['max', 'min']
     for k in k_values:  # iterate for every k in list
-        model = algorithm(k=k)  # algorithm params
+        model = algorithm(k=k, seed=seed)  # algorithm params
         model.fit(x)
         if algorithm == FuzzyCMeans:
-            y_pred= model.predict(x)[2]
+            y_pred = model.predict(x)[2]
         else:
             y_pred = model.predict(x)
         if metric in ['adjusted_mutual_info_score', 'adjusted_rand_score', 'completeness_score',
