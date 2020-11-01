6accf2ba64dd89f2e2f56e06b58f5f2810fc7da7
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-11-01 17:32:41 +0100

    Update main.py

--- w1/main.py
+++ w1/main.py
@@ -2,30 +2,54 @@ from w1.evaluation.main_functions import *
 
 
 def main():
-    # choose the dataset to analyze
-    database_name = "cmc"
+    """
+    choose the dataset to analyze.
+     OPTIONS:
+        -> "breast"
+        -> "cmc"
+        -> "adult"  (warning huge dataset, it can take some minutes to run all the algorithm)
+    """
+    database_name = "adult"
     res = preprocess_database(database_name)
 
-    # define the parameters
-    parameters = dict(k=2, max_it=100, seed=1, tol=1e-5, eps=0.5, epsilon=0.01, m=2, optimal_k=False)
+    """
+    define the input parameters for the algorithms:
+        k: number of clusters for KMeans, KMedian, BisectingKMeans and FuzzyCMeans algorithms.
+        max_it: maximum number of iterations.
+        seed: seed for the random search of first centroids.
+        tol: algorithms tolerance to movement of centroids between loops.
+        eps: epsilon for the DBScan algorithm.
+        epsilon: epsilon for FuzzyCMeans algorithm.
+        m: the fuzziness index for FuzzyCMeans.
+    """
+    parameters = dict(k=4, max_it=100, seed=1, tol=1e-5, eps=0.25, epsilon=0.01, m=2)
 
     # apply all the algorithms
     result = apply_algorithms(res['db'], res['label_true'], res['data_frame'], parameters)
 
-    # print result at the terminal
-    print(result['our_df'])
-    print(result['dbscan_df'])
-
     # load results to a csv file
-    result['our_df'].to_csv("results/"+database_name+"_algorithms_results", sep='\t', encoding='utf-8', index=False)
-    result['dbscan_df'].to_csv("results/"+database_name+"_dbscan_results", sep='\t', encoding='utf-8', index=False)
-    print("\nThe CSV output files are created in results folder of this project\n")
+    set_output(result, database_name)
 
+    """
+    define the input parameters for the optimize function, this function prints the optimal k value for each algorithm 
+    using une metric to evaluate this particularity
+        metric: that is the metric that you want to use to maximize or minimize to search the optimal K value, 
+        the available metrics are: 
+            adjusted_mutual_info_score,adjusted_rand_score, completeness_score, fowlkes_mallows_score,
+            homogeneity_score,v_measure_score, calinski_harabasz_score, davies_bouldin_score, silhouette_score,
+            normalized_partition_coefficient, partition_entropy, xie_beni
+        seed: seed for the random search of first centroids. It should be the same as the algorithm execution to extract
+        congruent results.
+        rang: list of K-values that you want to evaluate.
+        goal: depending of the metric, to obtain the optimal value you need to optimize or minimize the metric. 
+    """
     # find optimal K value for each algorithm
-    optim_k_value(res, database_name, parameters['seed'])
+    opt_param = dict(metric='silhouette_score', seed=parameters['seed'], rang=[2, 3, 4, 5, 6, 7, 8, 9, 10], goal='max')
+    optim_k_value(res, database_name, opt_param)
 
 
 if __name__ == "__main__":
     main()
+
 
 
