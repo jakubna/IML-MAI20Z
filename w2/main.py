4f452052d83df45be6be0e64d5f1567b84237382
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-11-13 19:46:54 +0100

    Update main.py

--- w2/main.py
+++ w2/main.py
@@ -9,7 +9,7 @@ def main():
         -> "cmc"
         -> "adult"  (warning huge dataset, it can take some minutes to run all the algorithm)
     """
-    database_name = "cmc"
+    database_name = "breast"
     res = preprocess_database(database_name)
 
     """
@@ -21,7 +21,7 @@ def main():
         n_components: number of components that finally you want to obtain after the pca reduction
         db_name: NON changeable item, is the used data set name
     """
-    parameters = dict(k=3, max_it=100, seed=1, tol=1e-5, n_components=3, db_name=database_name)
+    parameters = dict(k=len(set(res['label_true'])), max_it=100, seed=1, tol=1e-5, n_components=3, db_name=database_name)
 
     # make sure that the names of the rows are column names of the database and n_components=length of names_row
     components = get_features(res['data_frame'], parameters['n_components'])
@@ -29,10 +29,6 @@ def main():
     # apply all the algorithms
     apply_algorithms(res['db'], res['label_true'], parameters, components)
 
-    # load results to a csv file
-    # set_output(result, database_name)
-
 
 if __name__ == "__main__":
     main()
-
