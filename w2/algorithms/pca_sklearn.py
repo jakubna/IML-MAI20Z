1dd9f618c7cc05d442677eddcfe1b131fd67eba3
gabrielaleon <51418183+gabrielaleon@users.noreply.github.com>
2020-11-13 21:29:42 -0300

    Update pca_sklearn.py
    changing the n_compo 
    if int -> use the number provided
    if float -> estimate the mnumber according to the sum of variances ratio
    if nan -> n_compo = n_features

--- w2/algorithms/pca_sklearn.py
+++ w2/algorithms/pca_sklearn.py
@@ -11,19 +11,38 @@ def pca_sklearn(X, dataset_name, n_compo):
         n_compo: number of components that we want to reduce the X dataset.
         return: eigenvectors, eigenvalues , reduced data set
     """
-    pca = PCA(n_components=n_compo)
-    x_reduced = pca.fit_transform(X)
 
-    # compute the eigenvalues and eigenvectors and reduced data to the number of components that have eigenvalue
-    # bigger than 1 (more representative, has bigger variance)
-    biggest_eigenvalues = [i for i in pca.singular_values_ if i > 1]
-    x_pca = x_reduced[:, :len(biggest_eigenvalues)]
-    eigenvectors = pca.components_[:len(biggest_eigenvalues), :]
+    pca = PCA(n_components=X.shape[1])
+    x_reduced = pca.fit_transform(X)
+    print('SKLearn PCA')
+    print('All Eigenvalues:\n ', pca.explained_variance_)
+    print('All Eigenvectors:\n ', pca.components_)
+    eig_map = list(zip(pca.explained_variance_, pca.components_))
+    print('eig_map:\n',eig_map)
+
+    # compute the eigenvalues and eigenvectors and reduced data to the number of components that:
+    # 1) was required
+    # 2) represents n% of the explained variance
+    # 3) is equal to number of features
+    if type(n_compo) == int:
+        k=n_compo
+    elif type(n_compo) ==float:
+        k = np.searchsorted(pca.explained_variance_ratio_.cumsum(),n_compo) + 1
+    else:
+        k=X.shape[1]
+    print('k = ',k)
+    biggest_eigenvalues = pca.explained_variance_[:k]
+    x_pca = x_reduced[:, :k]
+    eigenvectors = pca.components_[:k, :]
+
+    print('K first eigenvalues:\n', biggest_eigenvalues)
+    print('K eigenvectors:\n', eigenvectors)
+
 
     # plot the components and eigenvalues to understand the number of optimal components
-    x = np.arange(len(pca.singular_values_))
+    x = np.arange(len(pca.explained_variance_))
     labels = [str(i + 1) + 'º Component' for i in list(x)]
-    plt.bar(x, pca.singular_values_)
+    plt.bar(x, pca.explained_variance_)
     plt.title('PCA - ' + dataset_name + ' data set')
     plt.ylabel('Eigenvalue')
     plt.xticks(x, labels)
@@ -40,21 +59,39 @@ def ipca_sklearn(X, dataset_name, n_compo):
         return: eigenvectors, eigenvalues , reduced data set
     """
 
-    ipca = IncrementalPCA(n_components=n_compo)
+    ipca = IncrementalPCA(X.shape[1])
     x_reduced_ipca = ipca.fit_transform(X)
-
-    # compute the eigenvalues and eigenvectors and reduced data to the number of components that have eigenvalue
+    print('SKLearn Incremental PCA')
+    print('All Eigenvalues:\n ', ipca.explained_variance_)
+    print('All Eigenvectors:\n ', ipca.components_)
+    eig_map = list(zip(ipca.explained_variance_, ipca.components_))
+    print('eig_map:\n',eig_map)
+    # compute the eigenvalues and eigenvectors and reduced data to the number of components that:
+    # 1) was required
+    # 2) represents n% of the explained variance
+    # 3) is equal to number of features
     # bigger than 1 (more representative, has bigger variance)
-    biggest_eigenvalues_ipca = [i for i in ipca.singular_values_ if i > 1]
-    x_ipca = x_reduced_ipca[:, :len(biggest_eigenvalues_ipca)]
-    eigenvectors_ipca = ipca.components_[:len(biggest_eigenvalues_ipca), :]
+
+    if type(n_compo) == int:
+        k=n_compo
+    elif type(n_compo) ==float:
+        k = np.searchsorted(ipca.explained_variance_ratio_.cumsum(),n_compo) + 1
+    else:
+        k=X.shape[1]
+    print('k = ', k)
+    biggest_eigenvalues_ipca = ipca.explained_variance_[:k]
+    x_ipca = x_reduced_ipca[:, :k]
+    eigenvectors_ipca = ipca.components_[:k, :]
+
+    print('K first eigenvalues:\n', biggest_eigenvalues_ipca)
+    print('K eigenvectors:\n', eigenvectors_ipca)
 
     # plot the components and eigenvalues to understand the number of optimal components
-    xipca = np.arange(len(ipca.singular_values_))
+    xipca = np.arange(len(ipca.explained_variance_))
     labels_ipca = [str(i + 1) + 'º Component' for i in list(xipca)]
     plt.title('Incremental PCA - ' + dataset_name + ' data set')
     plt.ylabel('Eigenvalue')
-    plt.bar(xipca, ipca.singular_values_)
+    plt.bar(xipca, ipca.explained_variance_)
     plt.xticks(xipca, labels_ipca)
     plt.show()
 
