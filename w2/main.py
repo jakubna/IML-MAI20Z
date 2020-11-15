c7dd352b09c77b1737b6bc5b4230cf2f14d5fec6
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-11-16 00:26:58 +0100

    Update main.py

--- w2/main.py
+++ w2/main.py
@@ -9,24 +9,25 @@ def main():
         -> "cmc"
         -> "adult"  (warning huge dataset, it can take some minutes to run all the algorithm)
     """
-    database_name = "breast"
+    database_name = "cmc"
     res = preprocess_database(database_name)
 
     """
     define the input parameters for the algorithms:
-        k: number of clusters for KMeans, KMedian, BisectingKMeans and FuzzyCMeans algorithms.
+        k: number of clusters for KMeans, in this Work 2 this parameter is fix. (NON MODIFICABLE)
+        db_name: the used data set name (NON MODIFICABLE)
         max_it: maximum number of iterations.
         seed: seed for the random search of first centroids.
         tol: algorithms tolerance to movement of centroids between loops.
         n_components: number of components that finally you want to obtain after the pca reduction or 
                         sum of the variance ratio to use as a threshold to determine the number of components
-                        if null, number of componentes=number of features        
-        db_name: NON changeable item, is the used data set name
+                        if null (or None), number of components=number of features.        
     """
-    parameters = dict(k=len(set(res['label_true'])), max_it=100, seed=1, tol=1e-5, n_components=0.9, db_name=database_name)
+    parameters = dict(max_it=100, seed=1, tol=1e-5, n_components=0.90,
+                      k=len(set(res['label_true'])), db_name=database_name)
 
     # make sure that the names of the rows are column names of the database and n_components=length of names_row
-    components = get_features(res['data_frame'])
+    components = get_features(res['data_frame'], parameters['n_components'])
 
     # apply all the algorithms
     apply_algorithms(res['db'], res['label_true'], parameters, components, database_name)
