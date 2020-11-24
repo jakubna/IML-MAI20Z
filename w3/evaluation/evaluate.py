bb08207b17fa9f364eb0a0a328a483f52f6b988b
gabrielaleon <51418183+gabrielaleon@users.noreply.github.com>
2020-11-24 11:17:56 -0300

    Update evaluate.py

--- w3/evaluation/evaluate.py
+++ w3/evaluation/evaluate.py
@@ -14,11 +14,11 @@ def evaluate_supervised_external(labels_true, labels_predicted):
         precision=precision_score(labels_true, labels_predicted),
         recall=recall_score(labels_true, labels_predicted),
         f1score=f1_score(labels_true, labels_predicted),
+        confusion_matrix=confusion_matrix(labels_true, labels_predicted),
+        classification_report=classification_report(labels_true, labels_predicted),
         adjusted_mutual_info_score=adjusted_mutual_info_score(labels_true, labels_predicted, 'arithmetic'),
         adjusted_rand_score=adjusted_rand_score(labels_true, labels_predicted),
         completeness_score=completeness_score(labels_true, labels_predicted),
-        confusion_matrix=confusion_matrix(labels_true, labels_predicted),
-        classification_report=classification_report(labels_true, labels_predicted),
         fowlkes_mallows_score=fowlkes_mallows_score(labels_true, labels_predicted),
         homogeneity_score=homogeneity_score(labels_true, labels_predicted),
         v_measure_score=v_measure_score(labels_true, labels_predicted))
