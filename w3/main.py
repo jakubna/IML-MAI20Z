6c549073601f742bad3581a3d23b781a5c2fdedf
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-15 00:52:27 +0100

    Update main.py

--- w3/main.py
+++ w3/main.py
@@ -1,4 +1,4 @@
-from w2.evaluation.main_functions import *
+from evaluation.main_functions import *
 
 
 def main():
@@ -12,25 +12,13 @@ def main():
     database_name = "cmc"
     res = preprocess_database(database_name)
 
-    """
-    define the input parameters for the algorithms:
-        k: number of clusters for KMeans, in this Work 2 this parameter is fix. (NON MODIFICABLE)
-        db_name: the used data set name (NON MODIFICABLE)
-        max_it: maximum number of iterations.
-        seed: seed for the random search of first centroids.
-        tol: algorithms tolerance to movement of centroids between loops.
-        n_components: number of components that finally you want to obtain after the pca reduction or 
-                        sum of the variance ratio to use as a threshold to determine the number of components
-                        if null (or None), number of components=number of features.        
-    """
-    parameters = dict(max_it=100, seed=1, tol=1e-5, n_components=0.90,
-                      k=len(set(res['label_true'])), db_name=database_name)
+    parameters = dict(n_neighbors=5, weights='equal', policy='majority_class', metric='minkowski')
 
     # make sure that the names of the rows are column names of the database and n_components=length of names_row
-    components = get_features(res['data_frame'], parameters['n_components'])
+    # components = get_features(res['data_frame'], parameters['n_components'])
 
     # apply all the algorithms
-    apply_algorithms(res['db'], res['label_true'], parameters, components, database_name)
+    apply_algorithms(res['db'], res['label_true'], parameters)
 
 
 if __name__ == "__main__":
