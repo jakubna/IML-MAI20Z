fff791d1d1becf0f281b1dc3080e95d7a6dc6a37
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-16 00:24:13 +0100

    Update main.py

--- w3/main.py
+++ w3/main.py
@@ -12,14 +12,13 @@ def main():
     database_name = "cmc"
     res = preprocess_database(database_name)
 
-    parameters = dict(n_neighbors=5, weights='equal', policy='majority_class', metric='minkowski')
-
     # make sure that the names of the rows are column names of the database and n_components=length of names_row
     # components = get_features(res['data_frame'], parameters['n_components'])
 
     # apply all the algorithms
-    apply_algorithms(res['db'], res['label_true'], parameters)
+    best_knn(res['db'], res['label_true'])
 
 
 if __name__ == "__main__":
     main()
+
