4c9caa4e4e9661d4588f9423ee17f4a12e1d3525
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-18 18:45:53 +0100

    Update main.py

--- w3/main.py
+++ w3/main.py
@@ -1,5 +1,5 @@
 from evaluation.main_functions import *
-from dataPreprocessing.read_data import read_and_preprocess_database as read_and_preprocess_database
+from dataPreprocessing.read_data import read_database as read_database
 
 
 def main():
@@ -9,14 +9,14 @@ def main():
         -> "grid"
         -> "hypothyroid"
     """
-    database_name = "grid"
-    res_db = read_and_preprocess_database(database_name)
+    database_name = "hypothyroid"
+    res_db = read_database(database_name)
 
     # name of the file where you want to store and read the metrics obtained
     n_file = "grid_metrics"
 
     # apply knn to all the possible combination of parameters
-    best_knn_metrics(res_db, n_file)
+    best_knn_metrics(res_db, n_file, database_name)
 
     # find the best knn parameter combination
     best_knn_get_best_comb(n_file)
