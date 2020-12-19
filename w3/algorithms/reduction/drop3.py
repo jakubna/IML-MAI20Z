4005191933051f040051cad4d7b0ec45dee379a9
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-19 22:28:33 +0100

    Update drop3.py

--- w3/algorithms/reduction/drop3.py
+++ w3/algorithms/reduction/drop3.py
@@ -2,19 +2,19 @@ import logging
 from typing import Tuple, List, Set
 import numpy as np
 import pandas as pd
-from algorithms.KNNAlgorithm import KNNLAlgorithm
+from algorithms.kNNAlgorithm import kNNAlgorithm
 from copy import deepcopy
 
 
-def drop3_reduction(knn: KNNLAlgorithm, X: np.ndarray, y: np.ndarray):
+def drop3_reduction(knn: kNNAlgorithm, X: np.ndarray, y: np.ndarray):
     S = list(range(X.shape[0]))
 
     remove_intances = []
     for i in S:
         dist, kn = knn.kneighbors(X[i, :])
-        kn=list(kn)
-        y_kn=[y[j] for j in kn]
-        if len(set(y_kn)) >1:
+        kn = list(kn)
+        y_kn = [y[j] for j in kn]
+        if len(set(y_kn)) > 1:
             remove_intances.append(i)
         else:
             if list(set(y_kn))[0] != y[i]:
@@ -25,17 +25,16 @@ def drop3_reduction(knn: KNNLAlgorithm, X: np.ndarray, y: np.ndarray):
         X = np.delete(X, remove_intances, axis=0)
         y = np.delete(y, remove_intances)
 
-
     S = list(range(X.shape[0]))
     associates: List[Set[int]] = [set() for _ in range(X.shape[0])]
     # neighbours = [[] for _ in range(S.shape[0])]
 
-    knn_1 = KNNLAlgorithm(K=knn.n_neighbors  + 1, policy=knn.policy, weights=knn.weights, metric=knn.metric)
-    knn_1.fit(X, y)
+    # knn_1 = kNNAlgorithm(K=knn.n_neighbors + 1, policy=knn.policy, weights=knn.weights, metric=knn.metric)
+    knn.fit(X, y)
 
     for p_idx in range(X.shape[0]):
         # Find the k + 1 nearest neighbors of p in S.
-        associates[p_idx] = set(knn_1.kneighbors(X[p_idx, :]))
+        associates[p_idx] = set(knn.kneighbors(X[p_idx, :]))
         logging.debug(f'Instance {p_idx} neighbours -> {associates[p_idx]}')
 
         # Add p to each of its neighbors’ lists of associates.
@@ -43,23 +42,24 @@ def drop3_reduction(knn: KNNLAlgorithm, X: np.ndarray, y: np.ndarray):
             associates[n_idx].add(p_idx)
             logging.debug(f'- Associates of {n_idx} -> {associates[n_idx]}')
 
-    # Order X from the element that has the furthest to the nearest nearest enemy (nearest neighbor that is of a different class), to do that:
-    min_dist=[]
-    intances=[]
+    # Order X from the element that has the furthest to the nearest nearest enemy
+    # (nearest neighbor that is of a different class), to do that:
+    min_dist = []
+    intances = []
     for p in range(X.shape[0]):
-        enemies=[]
-        yp=y[p]
+        enemies = []
+        yp = y[p]
         for i in range(X.shape[0]):
-            if y[i]!=yp:
+            if y[i] != yp:
                 enemies.append(i)
-        distances = knn._calculate_distance(X[p,:],X[enemies,:])
+        distances = knn._calculate_distance(X[p, :], X[enemies, :])
         min_dist.append(min(distances))
-        intances.append(X[p,:])
-    df=pd.DataFrame()
-    df['x']=intances
-    df['dist']=min_dist
-    df=df.sort_values(['dist'],ascending=False)
-    X=np.array(list(df['x']))
+        intances.append(X[p, :])
+    df = pd.DataFrame()
+    df['x'] = intances
+    df['dist'] = min_dist
+    df = df.sort_values(['dist'], ascending=False)
+    X = np.array(list(df['x']))
 
     p_idx = 0
     p_idx_original = 0
@@ -91,7 +91,7 @@ def drop3_reduction(knn: KNNLAlgorithm, X: np.ndarray, y: np.ndarray):
                 associates[a_idx] -= {p_idx_original}
 
                 # Find a new nearest neighbor for A.
-                a_nn = knn_1.kneighbors(X[a_idx, :])
+                a_nn = knn.kneighbors(X[a_idx, :])
 
                 # Add A to its new neighbor’s list of associates
                 associates[a_nn[0]].add(a_idx)
