67b4972be927d19a622964cdd826fe6780ac2227
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-19 22:19:57 +0100

    Update main.py

--- w3/main.py
+++ w3/main.py
@@ -13,17 +13,18 @@ def main():
     res_db = read_database(database_name)
 
     # name of the file where you want to store and read the metrics obtained
-    n_file = database_name+"_metrics"
+    n_file_metrics = database_name+"_metrics"
+    n_file_bests = database_name+"_bests_comb"
 
     # apply knn to all the possible combination of parameters
-    # best_knn_metrics(res_db, n_file, database_name)
+    # best_knn_metrics(res_db, database_name, database_name)
 
     # find the best knn parameter combination
-    best_knn_get_best_comb(n_file)
+    # best_knn_get_best_comb(n_file_metrics, n_file_bests)
 
     # get the reduction of the best knn
+    reduct_best_knn(n_file_bests, res_db, database_name)
 
 
 if __name__ == "__main__":
     main()
-
