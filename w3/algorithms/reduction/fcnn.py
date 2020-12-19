b934c51c93b006fc799965402db9fda4b0a61a6b
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-19 22:29:33 +0100

    Update fcnn.py

--- w3/algorithms/reduction/fcnn.py
+++ w3/algorithms/reduction/fcnn.py
@@ -1,19 +1,18 @@
 import numpy as np
-from algorithms.KNNAlgorithm import KNNAlgorithm
+from algorithms.kNNAlgorithm import kNNAlgorithm
 
 
-def fcnn_reduction(knn: KNNAlgorithm, X: np.ndarray, y: np.ndarray):
-    knn.fit(X,y)
+def fcnn_reduction(knn: kNNAlgorithm, X: np.ndarray, y: np.ndarray):
+    knn.fit(X, y)
     S, V = [], []
-    
 
-    #initialization with centroids of each different class
+    # initialization with centroids of each different class
     for label in set(y):
         i_labels = np.argwhere(y == label).reshape(-1)
         centroid = np.mean(np.array(X[i_labels, :]), axis=0)
         dist, ind = knn.kneighbors(centroid)
         if y[ind[0]] == label:
-            i=ind[0]              
+            i = ind[0]
         # Add to new sets
         S.append(X[i, :])
         V.append(y[i])
@@ -24,22 +23,22 @@ def fcnn_reduction(knn: KNNAlgorithm, X: np.ndarray, y: np.ndarray):
     S = np.array(S)
     V = np.array(V)
     knn.fit(S, V)
-    indices_to_remove=list(S)
+    # indices_to_remove = list(S)
     
     while True:
-        indices_to_remove=[]
+        indices_to_remove = []
         for i in range(S.shape[0]):
             # calculate distances between p = S[i,:] and others instances of S
-            distances_S = knn._calculate_distance(S, S[i,:])
-            distances_S = distances_S[distances_S != 0]
-            min_dist = distances_S.min()
-            distances_X=knn._calculate_distance(X, S[i,:])
-            for dist_id, dist in enumerate(distances_X):
-                if dist<min_dist:
+            distances_s = knn._calculate_distance(S, S[i, :])
+            distances_s = distances_s[distances_s != 0]
+            min_dist = distances_s.min()
+            distances_x = knn._calculate_distance(X, S[i, :])
+            for dist_id, dist in enumerate(distances_x):
+                if dist < min_dist:
                     if y[dist_id] != V[i]:
-                        indices_to_remove.append(dist_in)      
+                        indices_to_remove.append(dist_id)
         if len(indices_to_remove):
-            indices_to_remove=list(set(indices_to_remove))
+            indices_to_remove = list(set(indices_to_remove))
             # Update S and V
             S = np.vstack((S, X[indices_to_remove, :]))
             V = np.concatenate((V, y[indices_to_remove]))
