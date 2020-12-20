3bf584924dca7feb6829ffba321480c0bbd0362a
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-20 14:30:53 +0100

    Update main.py

--- w3/main.py
+++ w3/main.py
@@ -9,21 +9,30 @@ def main():
         -> "grid"
         -> "hypothyroid"
     """
-    database_name = "hypothyroid"
+    database_name = "grid"
     res_db = read_database(database_name)
 
     # name of the file where you want to store and read the metrics obtained
     n_file_metrics = database_name+"_metrics"
+    # name of the file where the dataframe of the possible combinations are ranked after statistical comparison
     n_file_bests = database_name+"_bests_comb"
+    # name of the file where you want to store and read the metrics obtained for the redacted knn.
+    n_file_redact_metrics = database_name + "_redact_metrics"
+    # name of the file where the dataframe of the redacted knn are ranked after statistical comparison
+    m_file_redact_bests = database_name+"redact_bests_comb"
 
-    # apply knn to all the possible combination of parameters
-    # best_knn_metrics(res_db, database_name, database_name)
+    # The following functions CAN BE EXECUTED ISOLATED, commenting the others
+    # apply knn to all the possible combination of parameters and store in a csv file
+    best_knn_metrics(res_db, n_file_metrics)
 
-    # find the best knn parameter combination
-    # best_knn_get_best_comb(n_file_metrics, n_file_bests)
+    # find the best knn parameter combination and store in a csv file
+    best_knn_statistical_comp(n_file_metrics, n_file_bests, False)
 
     # get the reduction of the best knn
-    reduct_best_knn(n_file_bests, res_db, database_name)
+    redact_best_knn(n_file_bests, n_file_redact_metrics, res_db)
+    
+    # get the rank between the redacted algorithms + non_redacted knn (with the best combination of parameters for knn)
+    best_knn_statistical_comp(n_file_redact_metrics, m_file_redact_bests, True)
 
 
 if __name__ == "__main__":
