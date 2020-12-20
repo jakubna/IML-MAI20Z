0eba4efcc71ea4bc78aa7f774ed30eeabc7846b2
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-21 00:29:44 +0100

    Update reductionKNNAlgorithm.py

--- w3/algorithms/reductionKNNAlgorithm.py
+++ w3/algorithms/reductionKNNAlgorithm.py
@@ -6,9 +6,17 @@ from algorithms.reduction.enn import enn_reduction
 from algorithms.reduction.menn import menn_reduction
 from algorithms.reduction.fcnn import fcnn_reduction
 
-reduction_methods = ['drop3', 'enn', 'fcnn', 'menn']
+reduction_methods = ['drop3', 'enn', 'fcnn', 'menn', 'full']
+
 
 def reduction_KNNAlogrithm(config: dict, X: np.ndarray, y: np.ndarray, reduction_method: str):
+    """
+        Apply the reduction to data passed by parameter.
+        :param reduction_method: algorithm chosen to make the reduction.
+        :param X: data ndarray with all the samples of the training part of the fold.
+        :param y: labels for each sample described in x.
+        :param config: Knn configuration that is set with parameters previously.
+    """
     if reduction_method not in reduction_methods:
         raise ValueError('Unknown reduction method')
     X = X.copy()
@@ -23,3 +31,5 @@ def reduction_KNNAlogrithm(config: dict, X: np.ndarray, y: np.ndarray, reduction
         return menn_reduction(alg, X, y)
     elif reduction_method == 'menn':
         return fcnn_reduction(alg, X, y)
+    elif reduction_method == 'full':
+        return X, y
