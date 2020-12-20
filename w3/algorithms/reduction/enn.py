b2f1d39c6d854029d202c77c6a2cc49e79990254
gabrielaleon <51418183+gabrielaleon@users.noreply.github.com>
2020-12-20 13:43:12 -0300

    Update enn.py

--- w3/algorithms/reduction/enn.py
+++ w3/algorithms/reduction/enn.py
@@ -6,8 +6,8 @@ def enn_reduction(knn: kNNAlgorithm, X: np.ndarray, y: np.ndarray):
     knn.fit(X, y)
     remove_intances = []
     for i in range(X.shape[0]):
-        pred = knn.predict([X[i, :]])
-        if pred != y[i]:
+        pred = knn.predict(np.array([X[i, :]]))
+        if pred[0] != y[i]:
             remove_intances.append(i)
     if len(remove_intances):
         # Remove instances from original data
