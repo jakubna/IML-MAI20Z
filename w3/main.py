74130cadaff939b9c8b7870dda21c3ad274782a0
Miquel Buxons Vives <63129841+MikiBuxons@users.noreply.github.com>
2020-12-21 13:46:59 +0100

    Update main.py

--- w3/main.py
+++ w3/main.py
@@ -12,28 +12,35 @@ def main():
     database_name = "grid"
     res_db = read_database(database_name)
 
-    # name of the file where you want to store and read the metrics obtained
+    # name of the file where are stored the metrics obtained by the execution of each possible combination of input
+    # parameters for KNN algorithm.
     n_file_metrics = database_name+"_metrics"
-    # name of the file where the dataframe of the possible combinations are ranked after statistical comparison
-    n_file_bests = database_name+"_bests_comb"
-    # name of the file where you want to store and read the metrics obtained for the redacted knn.
-    n_file_redact_metrics = database_name + "_redact_metrics"
-    # name of the file where the dataframe of the redacted knn are ranked after statistical comparison
-    m_file_redact_bests = database_name+"redact_bests_comb"
 
+    # name of the file where the Dataframe of the possible combinations are ranked after statistical comparison.
+    # Only the combinations that passed threshold of the test.
+    n_file_bests = database_name+"_bests_comb"
+
+    # name of the file where are stored the results of the execution of the 4 reduction algorithms that we choose
+    # (enn, menn, fcnn and drop3) and the non reduced one for the best combination of KNN parameters extracted before.
+    n_file_reduct_metrics = database_name + "_reduct_metrics"
+
+    # name of the file where are stored the results rank after the statistical analysis for the KNN results after
+    # applying the reduction algorithms. Only the algorithms that passed the test threshold.
+    n_file_reduct_bests = database_name+"_reduct_bests_comb"
+
+    ##############################################################################
     # The following functions CAN BE EXECUTED ISOLATED, commenting the others
     # apply knn to all the possible combination of parameters and store in a csv file
     best_knn_metrics(res_db, n_file_metrics)
 
     # find the best knn parameter combination and store in a csv file
-    best_knn_statistical_comp(n_file_metrics, n_file_bests, False)
+    best_knn_statistical_comp(n_file_metrics, n_file_bests, reduced=False)
 
     # get the reduction of the best knn
-    redact_best_knn(n_file_bests, n_file_redact_metrics, res_db)
+    reduct_best_knn(n_file_bests, n_file_reduct_metrics, res_db)
     
-    # get the rank between the redacted algorithms + non_redacted knn (with the best combination of parameters for knn)
-    best_knn_statistical_comp(n_file_redact_metrics, m_file_redact_bests, True)
-
+    # get the rank between the reduced algorithms + non_reduced knn (with the best combination of parameters for knn)
+    best_knn_statistical_comp(n_file_reduct_metrics, n_file_reduct_bests, reduced=True)
 
 if __name__ == "__main__":
     main()
